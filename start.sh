02a6206964ba6ab6c94d3435915bc88d5bd1ce94
Nikita Klyshko <n-klyshko@mail.ru>
2022-01-31 01:53:24 +0300

    Downgrade to 1.17, update config

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-java -Xms256M -Xmx8192M -Dfile.encoding=UTF-8 -jar paper-1.18.1-150.jar --log-count 0 --log-limit 1
+java -Xms256M -Xmx8192M -Dfile.encoding=UTF-8 -jar paper-1.17.1-408.jar --log-count 0 --log-limit 1
 
